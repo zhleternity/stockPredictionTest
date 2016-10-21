c796e57b031f1cf2cf2ac33420455796b91052d0
zhleternity <1021062123@qq.com>
2016-10-21 23:03:39 +0800

    hmm

---
+++ predictTest.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+from hmmlearn import hmm
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from sklearn.metrics.pairwise import pairwise_distances_argmin
